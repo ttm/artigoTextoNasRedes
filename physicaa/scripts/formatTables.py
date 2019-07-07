6a78db52eb8907dbd9f0fb768fee6215f0d59c69
Renato Fabbri <renato.fabbri@gmail.com>
2019-07-07 07:45:47 -0300

    arrumado artigo praticamente inteiro

---
+++ physicaa/scripts/formatTables.py
@@ -0,0 +1,28 @@
+import os
+
+tdir = '../tables/'
+ff = os.listdir(tdir)
+
+# no = [
+#  'begin{flushleft}',
+#  'Source: By the author',
+#  'end{flushleft}',
+# ]
+# 
+# for f in ff:
+#     with open(tdir + f, 'r') as f_:
+#         s = f_.readlines()
+#         for n in no:
+#             s = [l for l in s if n not in l]
+#         f_.close()
+#     with open(tdir + f, 'w') as f__:
+#         f__.writelines(s)
+
+for f in ff:
+    with open(tdir + f, 'r') as f_:
+        s = f_.readlines()
+        for n in no:
+            s = [l for l in s if n not in l]
+        f_.close()
+    with open(tdir + f, 'w') as f__:
+        f__.writelines(s)
